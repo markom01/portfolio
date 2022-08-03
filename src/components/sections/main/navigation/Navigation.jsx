6ec5a32094ad74bc44fbf66e2c68a3539fdcbc43
80217109 <markomesa01@gmail.com>
2022-08-03 15:12:47 +0200

    t4

--- src/components/sections/main/navigation/Navigation.jsx
+++ src/components/sections/main/navigation/Navigation.jsx
@@ -5,7 +5,7 @@ import { ImStack } from "react-icons/im"
 import { MdOutlineMonitor } from "react-icons/md"
 import { TbBriefcase } from "react-icons/tb"
 import { AiOutlineMessage } from "react-icons/ai"
-// import styles from "./Navigation.module.sass"
+import styles from "./Navigation.module.sass"
 
 const links = [
   {
@@ -30,20 +30,25 @@ export default function Navigation() {
   return (
     <div className="fixed-top col-2 h-100 col-md-1 border-start-0 d-flex flex-column justify-content-evenly align-items-center px-3 rounded-end glass overflow-auto">
       <a href="#hero">
-        <StaticImage
-          src="../../../../images/favicon.svg"
-          width={30}
-          alt="logo"
-        />
+        <StaticImage src="../../../../images/logo.svg" width={30} alt="logo" />
       </a>
-      {links.map((link) => (
-        <a className="d-flex flex-col" href={`#${link.name}`} key={link.name}>
-          <IconContext.Provider value={{ color: "white", size: "1.3em" }}>
+      <IconContext.Provider
+        value={{ color: "white", size: "1.3em" }}
+        className={styles.icons}
+      >
+        {links.map((link) => (
+          <a
+            className="text-center text-decoration-none"
+            href={`#${link.name}`}
+            key={link.name}
+          >
             {link.icon}
-          </IconContext.Provider>
-          <small>{link.name.toUpperCase()}</small>
-        </a>
-      ))}
+            <small className="d-block text-white mt-2">
+              {link.name.toUpperCase()}
+            </small>
+          </a>
+        ))}
+      </IconContext.Provider>
     </div>
   )
 }
