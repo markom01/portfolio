6725ecdfa2f8d490d45324dbd561fa5e6d346221
Marko Meseldžija <80217109+markom01@users.noreply.github.com>
2022-08-12 23:23:54 +0200

    Test codesandbox ide

--- src/components/sections/Sections.jsx
+++ src/components/sections/Sections.jsx
@@ -1,38 +1,30 @@
-import React, { useState, useEffect } from "react"
-import Fade from "react-reveal/Fade"
-import Scrollbar from "@myBlocks/scrollbar/Scrollbar"
-import Navigation from "@sections/navigation/Navigation"
-import Hero from "@sections/hero/Hero"
-import * as styles from "./Sections.module.sass"
-import BelowTheFold from "./BelowTheFold"
+import React, { useState, useEffect } from "react";
+import Fade from "react-reveal/Fade";
+import Scrollbar from "@myBlocks/scrollbar/Scrollbar";
+import Navigation from "@sections/navigation/Navigation";
+import Hero from "@sections/hero/Hero";
+import * as styles from "./Sections.module.sass";
+import BelowTheFold from "./BelowTheFold";
 
 export default function Sections() {
-  const [scrolled, setScrolled] = useState(0)
+  const [scrolled, setScrolled] = useState(0);
   const handleScroll = (e) => {
-    const target = e.currentTarget
-    const scrollTop = target.scrollTop
-    const targetHeight = target.scrollHeight - target.clientHeight
-    const scroll = `${(scrollTop / targetHeight) * 100}%`
-    setScrolled(scroll)
-  }
-  const [isWide, setIsWide] = useState(null)
-  useEffect(() => {
-    window.innerWidth > window.innerHeight ? setIsWide(true) : setIsWide(false)
-  }, [])
+    const target = e.currentTarget;
+    const scrollTop = target.scrollTop;
+    const targetHeight = target.scrollHeight - target.clientHeight;
+    const scroll = `${(scrollTop / targetHeight) * 100}%`;
+    setScrolled(scroll);
+  };
+
   return (
     <>
       <Scrollbar state={scrolled} />
-      <div
-        className={`vw-100 vh-100 pt-3 d-flex ${
-          isWide ? styles.pb : `flex-column-reverse`
-        }`}
-        id="main"
-      >
-        <Navigation wide={isWide} />
+      <div className={`vw-100 vh-100 pt-3 d-flex`} id="main">
+        <Navigation />
         <Fade>
           <div
             className={`glass mx-2  px-3  h-100 text-center 
-            ${isWide ? "mx-3" : "mb-3"}
+            }
       `}
             id="content"
             onScroll={handleScroll}
@@ -43,5 +35,5 @@ export default function Sections() {
         </Fade>
       </div>
     </>
-  )
+  );
 }
--- src/components/sections/navigation/Navigation.jsx
+++ src/components/sections/navigation/Navigation.jsx
@@ -1,12 +1,12 @@
-import React from "react"
-import Fade from "react-reveal/Fade"
-import { StaticImage } from "gatsby-plugin-image"
-import { IconContext } from "react-icons"
-import { ImStack } from "react-icons/im"
-import { MdOutlineMonitor } from "react-icons/md"
-import { TbBriefcase } from "react-icons/tb"
-import { AiOutlineMessage } from "react-icons/ai"
-import * as styles from "./Navigation.module.sass"
+import React from "react";
+import Fade from "react-reveal/Fade";
+import { StaticImage } from "gatsby-plugin-image";
+import { IconContext } from "react-icons";
+import { ImStack } from "react-icons/im";
+import { MdOutlineMonitor } from "react-icons/md";
+import { TbBriefcase } from "react-icons/tb";
+import { AiOutlineMessage } from "react-icons/ai";
+import * as styles from "./Navigation.module.sass";
 
 const links = [
   {
@@ -18,57 +18,47 @@ const links = [
         alt="logo"
         className={styles.logo}
       />
-    ),
+    )
   },
   {
     name: "skills",
-    icon: <ImStack />,
+    icon: <ImStack />
   },
   {
     name: "projects",
-    icon: <MdOutlineMonitor />,
+    icon: <MdOutlineMonitor />
   },
   {
     name: "experience",
-    icon: <TbBriefcase />,
+    icon: <TbBriefcase />
   },
   {
     name: "contact",
-    icon: <AiOutlineMessage />,
-  },
-]
+    icon: <AiOutlineMessage />
+  }
+];
 
-export default function Navigation({ wide }) {
+export default function Navigation() {
   return (
-    <Fade left={wide} bottom={!wide}>
+    <Fade>
       <nav
-        className={`d-flex justify-content-evenly align-items-center glass ${
-          styles.nav
-        }  
-      ${
-        wide
-          ? `flex-column  border-start-0 px-2`
-          : `border-bottom-0 px-3 py-2 mx-2`
-      }`}
+        className={`d-flex justify-content-evenly align-items-center glass ${styles.nav}  
+      `}
         id="nav"
       >
         <IconContext.Provider
           value={{
-            size: "1.3em",
+            size: "1.3em"
           }}
         >
           {links.map((link) => (
             <a
-              className={`text-center text-decoration-none py-1 px-2 w-100 ${
-                wide ? "" : `px-2`
-              }`}
+              className={`text-center text-decoration-none py-1 px-2 w-100`}
               href={`#${link.name}`}
               key={link.name}
             >
               <span
-                className={`d-flex align-items-center justify-content-center ${
-                  wide ? `p-3` : "p-2"
-                }`}
+                className={`d-flex align-items-center justify-content-center`}
                 style={{ color: "var(--icons-color)" }}
               >
                 {link.icon}
@@ -84,5 +74,5 @@ export default function Navigation({ wide }) {
         </IconContext.Provider>
       </nav>
     </Fade>
-  )
+  );
 }
