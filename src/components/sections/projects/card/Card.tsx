90b1f2597a1c81dc1f533473b0f1dc1b84b597f0
Marko Meseldžija <markomesa01@gmail.com>
2022-10-13 11:15:12 +0000

    test card dragging

--- src/components/sections/projects/card/Card.tsx
+++ src/components/sections/projects/card/Card.tsx
@@ -18,17 +18,42 @@ import Icon from "../../../blocks/myBlocks/Icon";
 type CardProps = Queries.ProjectsQuery["allMdx"]["nodes"][0];
 
 export default function Card({ project }: { project: CardProps }) {
-  const [showBack, setShowBack] = useState(false);
-
+  // const [showBack, setShowBack] = useState(false);
+  const [rotation, setRotation] = useState(0);
+  const [translation, setTranslation] = useState(0);
+  const handleTransform = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const { clientX } = e;
+    const { right, width } = e.currentTarget.getBoundingClientRect();
+    const x = right - clientX;
+    const RATIO = x / width;
+    // const isLeft = x < width / 2;
+    // const isRight = x > width / 2;
+    setTranslation(RATIO * 100);
+    setRotation(RATIO * 180);
+  };
+  const handleMouseLeave = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (translation < 50) {
+      setRotation(0);
+      setTranslation(0);
+    } else {
+      setRotation(180);
+      setTranslation(100);
+    }
+  };
   return (
     <div className={`col ${styles.scene}`}>
       <div
-        className={`h-100 w-100 position-relative my-card ${styles.card} ${
-          showBack ? styles.show_back : ""
-        }`}
+        className={`h-100 w-100 position-relative my-card ${styles.card}`}
+        onMouseMove={handleTransform}
+        onMouseLeave={handleMouseLeave}
+        style={{
+          transform: `translate3d(-${translation}%, 0, 0) rotate3d(0, 1, 0, -${rotation}deg) `,
+        }}
       >
-        <CardFront project={project} state={[showBack, setShowBack]} />
-        <CardBack project={project} state={[showBack, setShowBack]} />
+        <CardFront project={project} />
+        <CardBack project={project} />
       </div>
     </div>
   );
@@ -36,10 +61,9 @@ export default function Card({ project }: { project: CardProps }) {
 
 interface CardSidesProps {
   project: CardProps;
-  state: [boolean, React.Dispatch<React.SetStateAction<boolean>>];
 }
 
-function CardFront({ project, state }: CardSidesProps) {
+function CardFront({ project }: CardSidesProps) {
   const thumbnail = getImage(project.frontmatter.thumbnail);
   // const [isCarouselVisibleState, setisCarouselVisibleState] = useState(false)
   return (
@@ -47,7 +71,6 @@ function CardFront({ project, state }: CardSidesProps) {
       className={`${styles.glassCard} d-flex flex-column align-items-center position-absolute ${styles.card__face} ${styles.card__facefront}`}
     >
       {
-      
         <Fade cascade>
           {/* <div
           className={`w-100 h-100 d-flex justify-content-center align-items-center position-absolute ${styles.img_overlay}`}
@@ -90,7 +113,7 @@ function CardFront({ project, state }: CardSidesProps) {
               </div>
               <button
                 className={`text-white ${styles.flip_btn}`}
-                onClick={() => state[1]((s) => !s)}
+                // onClick={() => state[1]((s) => !s)}
                 data-tooltip="Show More"
               >
                 <Icon name="chevron-right" />
@@ -102,43 +125,39 @@ function CardFront({ project, state }: CardSidesProps) {
     </div>
   );
 }
-function CardBack({ project, state }: CardSidesProps) {
+function CardBack({ project }: CardSidesProps) {
   const startDate = new Date(project.frontmatter.startDate);
   const endDate = new Date(project.frontmatter.endDate);
   return (
     <div
       className={`${styles.glassCard} p-3 d-flex flex-column align-items-center position-absolute text-start ${styles.card__face} ${styles.card__faceback}`}
     >
-      
-        
-        <Fade cascade>
-          <div className="d-flex flex-column h-100">
-            <h5 className="mb-2 text-white">Description</h5>
-            <h6>{project.frontmatter.description}</h6>
-          </div>
-          <div className="d-flex w-100 justify-content-between align-items-center">
-            <button
-              className={styles.flip_btn}
-              onClick={() => state[1]((s) => !s)}
-            >
-              <Icon name="chevron-left" />
-            </button>
-            <h6
-              data-tooltip="Project Duration"
-              className="d-flex align-items-center mb-0"
-              style={{ fontSize: "14px" }}
-            >
-              <Icon name="calendar" className="me-2" />
-              <span>
-                {`${startDate.getMonth() + 1}.${startDate.getFullYear()} - ${
-                  endDate.getMonth() + 1
-                }.${endDate.getFullYear()}`}
-              </span>
-            </h6>
-          </div>
-        </Fade>
-        
-    
+      <Fade cascade>
+        <div className="d-flex flex-column h-100">
+          <h5 className="mb-2 text-white">Description</h5>
+          <h6>{project.frontmatter.description}</h6>
+        </div>
+        <div className="d-flex w-100 justify-content-between align-items-center">
+          <button
+            className={styles.flip_btn}
+            // onClick={() => state[1]((s) => !s)}
+          >
+            <Icon name="chevron-left" />
+          </button>
+          <h6
+            data-tooltip="Project Duration"
+            className="d-flex align-items-center mb-0"
+            style={{ fontSize: "14px" }}
+          >
+            <Icon name="calendar" className="me-2" />
+            <span>
+              {`${startDate.getMonth() + 1}.${startDate.getFullYear()} - ${
+                endDate.getMonth() + 1
+              }.${endDate.getFullYear()}`}
+            </span>
+          </h6>
+        </div>
+      </Fade>
     </div>
   );
 }
